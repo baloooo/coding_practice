50ca4afc8ebfb7a2e80c13564b533fdcecf6e9e9
baloooo <discoverx1@gmail.com>
2017-01-11 19:08:59 +0900

    uncomplete reverse_linklist2

--- reverse_linked_list_advanced.py
+++ reverse_linked_list_advanced.py
@@ -1,33 +1,42 @@
-class Node(object):
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next = next
+"""
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
 
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
 
-def construct_linked_list_from_array(inp_arr):
-    if not inp_arr:
-        return
-    head = Node(inp_arr[0])
-    prev = head
-    for each in inp_arr[1:]:
-        node = Node(each)
-        prev.next = node
-        prev = node
-    return head
+return 1->4->3->2->5->NULL.
+"""
 
+from linkedlistbase import (
+    Node, construct_linked_list_from_array,
+    print_linked_list)
 
-def print_linked_list(head):
-    while(head):
-        print head.val,
-        head = head.next
 
 def reverse_linked_list_with_params(A, m, n):
-    temp_head = A
-    linked_list_length = 0
-    while(temp_head.next is not None):
-        linked_list_length+=1
+    original_head = head = A
+    for i in xrange(1, m-1):
+        head = head.next
+    lower_half = head
+    if lower_half.next is None:
+        return lower_half
+    print 'lower half starts here', head.val
+    last = None
+    cur = head
+    nxt = reverse_pipe_end_node = cur.next
+    print 'm: %d, n: %d' % (m, n)
+    # for i in xrange(m, n+2):
+    i = m
+    while(i <= n):
+        print 'i: ', i
+        nxt = cur.next
+        cur.next = last
+        last = cur
+        cur = nxt
+        i += 1
+    reverse_pipe_end_node.next = cur
+    lower_half.next = last
+    return original_head
 
-    if 1<m<n<linked_list_length:
 
 def reverse_linked_list_optimized(A):
     # Most optimized solution
@@ -40,6 +49,8 @@ def reverse_linked_list_optimized(A):
         last = cur
         cur = nxt
     return last
+
+
 def reverse_linked_list(head):
     # First shot at reversing linked list
     # node for 1 and 2 nodes
@@ -53,14 +64,14 @@ def reverse_linked_list(head):
     # code for greater than 2 nodes
     current_node = head
     next_node = None
-    while((current_node.next is not None) and (current_node.next.next is not None)):
+    while((current_node.next is not None) and
+          (current_node.next.next is not None)):
         jump_node = current_node.next.next
         next_node_backup = next_node
         next_node = current_node.next
         current_node.next = next_node_backup
         next_node.next = current_node
         current_node = jump_node
-    # import ipdb; ipdb.set_trace()
     if current_node.next is None:
         current_node.next = next_node
         new_head = current_node
@@ -74,11 +85,15 @@ def reverse_linked_list(head):
 
 
 if __name__ == '__main__':
-    # inp_arr = [1, 2, 3, 4, 5]
+    inp_arr = [1, 2, 3, 4]
     # inp_arr = [1, 2, 3, 4, 5, 6]
     # inp_arr = [1]
-    inp_arr = [1, 2]
+    # inp_arr = [1, 2]
+    # inp_arr = [1, 2, 3]
+    m = 1
+    n = 4
     head = construct_linked_list_from_array(inp_arr)
     print_linked_list(head)
-    print 
-    print_linked_list(reverse_linked_list(head))
+    print
+    print_linked_list(reverse_linked_list_with_params(head, m, n))
+    # print_linked_list(reverse_linked_list(head))
