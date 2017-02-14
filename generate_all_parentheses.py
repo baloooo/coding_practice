9a9cc7c6f3b7c0e98e125130b46dfba6a5ceb557
baloooo <discoverx1@gmail.com>
2017-02-14 23:02:33 +0900

    hamming_distance

--- generate_all_parentheses.py
+++ generate_all_parentheses.py
@@ -12,23 +12,40 @@ class Solution():
         self.cur_paran_combination = []
         self.balanced_paran = []
 
+    def gen_balanced_paran_simplified(self, n):
+        if n<1:
+            return []
+        def gen_paran(left_paran_stock, right_paran_stock):
+            if left_paran_stock == 0 and right_paran_stock == 0:
+                self.balanced_paran.append(''.join(self.cur_paran_combination))
+            if left_paran_stock != 0:
+                self.cur_paran_combination.append('(')
+                gen_paran(left_paran_stock-1, right_paran_stock+1)
+                self.cur_paran_combination.pop()
+            if right_paran_stock != 0:
+                self.cur_paran_combination.append(')')
+                gen_paran(left_paran_stock, right_paran_stock-1)
+                self.cur_paran_combination.pop()
+        gen_paran(3, 0)
+        return self.balanced_paran
+
     def gen_balanced_paran(self, n):
-        def gen_paran(left_paran, right_paran):
-            if left_paran == 0 and right_paran == 0:
+        def gen_paran(left_paran_stock, right_paran_stock):
+            if left_paran_stock == 0 and right_paran_stock == 0:
                 # add balanced condn
                 self.balanced_paran.append(''.join(self.cur_paran_combination))
                 # self.cur_paran_combination.pop()
             else:
                 # conditions for keeping paran balanced
                 # don't make left paran if no right paran left to cover it up
-                if left_paran != 0 and right_paran != 0:
+                if left_paran_stock != 0 and right_paran_stock != 0:
                     self.cur_paran_combination.append('(')
-                    gen_paran(left_paran-1, right_paran)
+                    gen_paran(left_paran_stock-1, right_paran_stock)
                     self.cur_paran_combination.pop()
                 # right paran should be always greater than left param to finally cover all left param added
-                if right_paran != 0 and right_paran > left_paran:
+                if right_paran_stock != 0 and right_paran_stock > left_paran_stock:
                     self.cur_paran_combination.append(')')
-                    gen_paran(left_paran, right_paran-1)
+                    gen_paran(left_paran_stock, right_paran_stock-1)
                     self.cur_paran_combination.pop()
         if n < 1:
             return []
@@ -42,3 +59,4 @@ if __name__ == '__main__':
     n = 0
     n = 3
     print sol.gen_balanced_paran(n)
+    # print sol.gen_balanced_paran_simplified(n)
---
+++ hamming_distance.py
@@ -0,0 +1,35 @@
+# *-* coding: utf-8
+# Time:  O(1)
+# Space: O(1)
+
+# The Hamming distance between two integers is the number of positions
+# at which the corresponding bits are different.
+#
+# Given two integers x and y, calculate the Hamming distance.
+#
+# Note:
+# 0 ≤ x, y < 231.
+#
+# Example:
+#
+# Input: x = 1, y = 4
+#
+# Output: 2
+#
+# Explanation:
+# 1   (0 0 0 1)
+# 4   (0 1 0 0)
+#       ↑   ↑
+#
+# The above arrows point to positions where the corresponding bits are different.
+
+def hamming_distance(num1, num2):
+    # integer to binary conversion http://stackoverflow.com/questions/16926130/python-convert-to-binary-and-keep-leading-zeros
+    bin1 = '{0:015b}'.format(num1)
+    bin2 = '{0:015b}'.format(num2)
+    # print bin1, bin2
+    return (sum(cur_bit_num1!=cur_bit_num2 for cur_bit_num1, cur_bit_num2 in zip(bin1, bin2)))
+
+if __name__ == '__main__':
+    num1, num2 = 1, 4
+    print hamming_distance(num1, num2)
