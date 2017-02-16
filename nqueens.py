cd6e9207ad24e4fec24939c30feadb20404fbde7
baloooo <discoverx1@gmail.com>
2017-02-16 22:01:24 +0900

    ncr and nqueen

--- nqueens.py
+++ nqueens.py
@@ -29,13 +29,14 @@ class Solution():
         self.cur_arrng = []
 
     def possible_nqueens(self, n):
+        from copy import deepcopy
         def no_conflicting_queen(row, col):
             # check column
-            for cur_row in xrange(n):
+            for cur_row in xrange(row+1):
                 if self.cur_arrng[cur_row][col] == 'Q':
                     return False
             # check row
-            for cur_col in xrange(n):
+            for cur_col in xrange(col+1):
                 if self.cur_arrng[row][cur_col] == 'Q':
                     return False
             # check diagonal
@@ -53,25 +54,11 @@ class Solution():
                     return False
                 cur_row -= 1
                 cur_col += 1
-            # # bottom right
-            # cur_row, cur_col = row, col
-            # while(cur_row < n and cur_col < n):
-            #     if self.cur_arrng[cur_row][cur_col] == 'Q':
-            #         return False
-            #     cur_row += 1
-            #     cur_col += 1
-            # # bottom left
-            # cur_row, cur_col = row, col
-            # while(cur_row < n and cur_col >= 0):
-            #     if self.cur_arrng[cur_row][cur_col] == 'Q':
-            #         return False
-            #     cur_row += 1
-            #     cur_col -= 1
             return True
 
         def nqueen(row):
             if row == n:
-                self.possible_arrangements.append(self.cur_arrng[::])
+                self.possible_arrangements.append(deepcopy(self.cur_arrng))
             else:
                 for col in xrange(n):
                     if no_conflicting_queen(row, col):
@@ -82,11 +69,14 @@ class Solution():
             return []
         self.cur_arrng = [['.']*n for _ in xrange(n)]
         nqueen(0)
-        return self.possible_arrangements
+        result = [[''.join(each) for each in combination] for combination in self.possible_arrangements]
+        if len(result) == 1:
+            return result[0]
+        return result
 
 if __name__ == '__main__':
     sol = Solution()
-    n = 5
+    n = 4
     for nqueen_combination in sol.possible_nqueens(n):
         for each in nqueen_combination:
             print each
