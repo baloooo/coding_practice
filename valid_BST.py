f5c7b3905fd8de4d9f366a667c2229729046a1b4
baloooo <discoverx1@gmail.com>
2017-02-24 17:20:37 +0900

    tree_base and valid_bst incomplete

---
+++ valid_BST.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8
+"""
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the
+node’s key.
+The right subtree of a node contains only nodes with keys greater than the
+node’s key.
+Both the left and right subtrees must also be binary search trees.
+Example :
+
+Input :
+   1
+  /  \
+ 2    3
+
+Output : 0 or False
+
+
+Input :
+  2
+ / \
+1   3
+
+Output : 1 or True
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+"""
+from tree_base import array_to_tree, print_tree_dfs
+
+
+def is_bst(root):
+    pass
+
+if __name__ == '__main__':
+    arr = [1, 5, 5, 2, 2, -1, 3]
+    root = array_to_tree(arr)
+    print_tree_dfs(root)
+    is_bst(root)
