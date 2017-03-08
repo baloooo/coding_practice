02294d687230ecc5c26f75f7a79e373f03e99182
baloooo <discoverx1@gmail.com>
2017-03-08 22:01:33 +0900

    inorder_traversal

--- inorder_traversal.py
+++ inorder_traversal.py
@@ -28,23 +28,30 @@ def inorder_iteration(root):
     from tree_base import Node
     stack = [root]
     tos = None
-    while(stack):
+    in_order = []
+    while(stack or tos is not None):
         if tos is None:
             tos = stack.pop()
         if not isinstance(tos, Node):
-            print tos
-            tos = stack.pop()
+            in_order.append(tos)
+            try:
+                tos = stack.pop()
+            except IndexError:
+                tos = None
             continue
         if tos.right is not None:
             stack.append(tos.right)
         stack.append(tos.val)
         tos = tos.left
+    return in_order
 
 
 
 if __name__ == '__main__':
     from tree_base import level_order_array_to_tree
     # arr = [1, None, 2, None, None, 3, None]
-    arr = ['A', 'B', 'C', 'D', "E", "F", "G"]
+    # arr = ['A', 'B', 'C', 'D', "E", "F", "G"]
+    # arr = ['A', 'B', 'C']
+    arr = ['a', 'b']
     root = level_order_array_to_tree(arr)
     print inorder_iteration(root)
