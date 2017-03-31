5c9046a9648e5b006e4039682348881321d8dcac
baloooo <discoverx1@gmail.com>
2017-03-31 18:18:13 +0900

    max_contiguous_sub_array

--- max_contiguous_sub_array.py
+++ max_contiguous_sub_array.py
@@ -1,7 +1,41 @@
-import sys
+"""
+Max Sum Contiguous SubarrayBookmark Suggest Edit
+Find the contiguous subarray within an array (containing at least one number)
+which has the largest sum.
 
-max_until_here = max_so_far = -sys.maxint-1
-for x in A:
-    max_until_here = max(x, max_until_here)
-    max_so_far = max(max_until_here, max_so_far)
-print max_so_far
+For example:
+
+Given the array [-2,1,-3,4,-1,2,1,-5,4],
+
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+For this problem, return the maximum sum.
+https://www.youtube.com/watch?v=86CQq3pKSUw
+"""
+
+
+class Solution:
+    def max_sub_array(self, arr):
+        global_max = current_max = arr[0]
+        i, j = 0, 0
+        for index in xrange(1, len(arr)):
+            # current_max = max(arr[index], current_max+arr[index])
+            new_i, new_j = 0, 0
+            if arr[index] > current_max+arr[index]:
+                new_i = new_j = index
+                current_max = arr[index]
+            else:
+                current_max = current_max+arr[index]
+                new_i = i
+                new_j = j + 1
+            if global_max < current_max:
+                i, j = new_i, new_j
+                global_max = current_max
+            # global_max = max(current_max, global_max)
+        # optionally sends back array indexes also.
+        return (global_max, (i, j))
+
+if __name__ == '__main__':
+    arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    # arr = [100, -3, -4, -5, -2, 2]
+    print Solution().max_sub_array(arr)
--- sub_matrices_with_sum_zero.py
+++ sub_matrices_with_sum_zero.py
@@ -29,10 +29,19 @@ class Solution:
     def __init__(self):
         pass
 
-    def sub_matrices_sum_zero(matrix):
-        for row in matrix:
-            for col in row:
+    def sub_matrices_sum_zero(self, matrix):
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in xrange(row):
+            for j in xrange(i, row):
+                for m in xrange(col):
+                    for n in xrange(m, col):
+                        print "matrix:"
+                        for row_index in xrange(i):
+                            for col_index in xrange(m):
+                                print matrix[row_index+j][col_index+n]
+                            print
 
 if __name__ == '__main__':
-    matrix = [[-8, 5, 7], [3, 7, -8], [5, -8,  9]]
-    print Solution().sub_matrices_sum_zero(matrix)
+    test_case = [[-8, 5, 7], [3, 7, -8], [5, -8,  9]]
+    print Solution().sub_matrices_sum_zero(test_case)
