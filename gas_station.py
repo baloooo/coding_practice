d8ba476ee7648839846aea7de6b0e39b7a980c5b
baloooo <discoverx1@gmail.com>
2017-04-19 19:08:30 +0900

    gas_station, subset_sum

---
+++ gas_station.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+"""
+There are N gas stations along a circular route, where the amount of gas at
+station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to
+travel from station i to its next station (i+1). You begin the journey with
+an empty tank at one of the gas stations.
+
+Return the minimum starting gas station’s index if you can travel around the
+circuit once, otherwise return -1.
+
+You can only travel in one direction. i to i+1, i+2, ... n-1, 0, 1, 2..
+Completing the circuit means starting at i and ending up at i again.
+
+Example :
+
+Input :
+      Gas :   [1, 2]
+            Cost :  [2, 1]
+
+            Output : 1
+
+If you start from index 0, you can fill in gas[0] = 1 amount of gas.
+Now your tank has 1 unit of gas. But you need cost[0] = 2 gas to travel to
+station 1.
+If you start from index 1, you can fill in gas[1] = 2 amount of gas.
+Now your tank has 2 units of gas. You need cost[1] = 1 gas to get to station 0.
+So, you travel to station 0 and still have 1 unit of gas left over.
+You fill in gas[0] = 1 unit of additional gas, making your current gas = 2.
+It costs you cost[0] = 2 to get to station 1, which you do and complete the
+circuit.
+"""
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def gas_station_route_possible_optimized(self, gas_arr, cost_arr):
+        """
+        Idea:
+        1. If car starts at A and can not reach B. Any station between A and B
+           can not reach B.(B is the first station that A can not reach.)
+        2. If the total number of gas is bigger than the total number of cost.
+           There must be a solution.
+        """
+        start, total, tank = 0, 0, 0
+        for i in xrange(len(gas_arr)):
+            tank += gas_arr[i] - cost_arr[i]
+            if tank < 0:
+                start = i + 1
+                # This accumulates all the deficiency in the gas till reset point  # noqa
+                total += tank
+                tank = 0
+        # Can total(deficiencies till last reset point) +
+        # tank (existing gas in tank) be greater than equal to zero i.e possible to cover.  # noqa
+        return -1 if (total + tank) < 0 else start
+
+    def gas_station_route_possible(self, gas_arr, cost_arr):
+        # Time: O(n^2) Space: O(1)
+        # Total Gas should be greater than total cost
+        if sum(gas_arr) < sum(cost_arr):
+            return -1
+        for index in xrange(len(gas_arr)):
+            cur_gas = 0
+            cur_gas = gas_arr[index] - cost_arr[index]
+            cur_index = index+1
+            if cur_gas < 0:
+                continue
+            while(cur_index % len(gas_arr) != index):
+                cur_gas += gas_arr[cur_index % len(gas_arr)] - cost_arr[cur_index % len(gas_arr)]  # noqa
+                cur_index = cur_index+1
+                if cur_gas < 0:
+                    break
+            else:
+                return index
+        return -1
+
+if __name__ == '__main__':
+    # gas_arr, cost_arr = [1, 2], [2, 1]
+    # gas_arr, cost_arr = [2, 4], [3, 4]
+    gas_arr, cost_arr = [4], [5]
+    print Solution().gas_station_route_possible(gas_arr, cost_arr)
--- subset_sum.py
+++ subset_sum.py
@@ -11,16 +11,34 @@ class Solution:
     def __init__(self):
         pass
 
-    def subset_sum(self, arr, arr_len, cur_sum):
+    def subset_sum_recursion(self, arr, arr_len, cur_sum):
         if cur_sum == 0:
             return True
         if arr_len < 0 and cur_sum > 0:
             return False
         if cur_sum < 0:
             return False
-        return (self.subset_sum(arr, arr_len - 1, cur_sum) or
-                self.subset_sum(arr, arr_len - 1, cur_sum-arr[arr_len-1]))
+        return (self.subset_sum_recursion(arr, arr_len - 1, cur_sum) or
+                self.subset_sum_recursion(arr, arr_len - 1,
+                                          cur_sum-arr[arr_len-1]))
+
+    def subset_sum_dp(self, arr, target_sum):
+        # dp[i][j] can we make cur_sum (i) using numbers 0 to j-1
+        # dp = [i (cur_sum)][numbers 0 ... j-1]
+        dp = [[False for _ in xrange(len(arr)+1)] for _ in xrange(target_sum+1)]  # noqa
+        for i in xrange(target_sum+1):
+            dp[i][0] = False
+        for j in xrange(len(arr)+1):
+            dp[0][j] = True
+        for cur_sum in xrange(1, target_sum+1):
+            for cur_ele in xrange(1, len(arr)+1):
+                dp[cur_sum][cur_ele] = dp[cur_sum][cur_ele-1]
+                # cur_sum >= cur_element as only then can we try to make cur_sum using cur_element.  # noqa
+                if cur_sum >= arr[cur_ele-1]:
+                    dp[cur_sum][cur_ele] = dp[cur_sum][cur_ele] or dp[cur_sum - arr[cur_ele-1]][cur_ele-1]  # noqa
+        return dp[target_sum][len(arr)]
 
 if __name__ == '__main__':
     arr, target_sum = [3, 34, 4, 12, 5, 2], 9
-    print Solution().subset_sum(arr, len(arr), target_sum)
+    print Solution().subset_sum_recursion(arr, len(arr), target_sum)
+    print Solution().subset_sum_dp(arr, target_sum)
