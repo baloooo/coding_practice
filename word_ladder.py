a6acd393c5272abc050f462543f6b5dc5e3ff188
baloooo <discoverx1@gmail.com>
2017-05-19 18:36:01 +0900

    word_ladder and zero_one_multiple

---
+++ word_ladder.py
@@ -0,0 +1,150 @@
+"""
+Given two words (start and end), and a dictionary, find the length of shortest
+transformation sequence from start to end, such that:
+
+You must change exactly one character in every transformation
+Each intermediate word must exist in the dictionary
+Example :
+
+Given:
+
+start = "hit"
+end = "cog"
+dict = ["hot","dot","dog","lot","log"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note that we account for the length of the transformation path instead of the
+number of transformation itself.
+
+ Note:
+Return 0 if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+"""
+
+
+class Solution:
+    """
+    Time: O(n^2*m) where n is the number of entries in word_list
+    and m is the size of goal_word (sizes of all words are same though)
+    We'll be iterating over word_list for a max of n^2 times, worst case
+    being when for every iteration only one word is added to queue and
+    we iterated the entire list so put all elements from word_list to
+    queue we'll require n^2 iterations (putting one element per iteration)
+    word_list size-> n -> n-1 -> n-2 -> ... and each iteration requires full
+    iteration over word list.
+    Now for each iteration we call is_adjacent which takes O(m) time which
+    essentially is just one pass over cur_word and target_word, so in total
+    overall time complexity: O(n^2*m)
+    """
+    def __init__(self):
+        pass
+
+    def is_adjacent(self, test_word, cur_word):
+        # to store count of differences
+        count = 0
+        # iterate thru all chars and return false if
+        # more than one mismatching characters.
+        # Note: length of all words in list is same
+        for index in xrange(len(test_word)):
+            if test_word[index] != cur_word[index]:
+                count += 1
+            if count > 1:
+                return False
+        """
+        To ignore count == 0 which would mean strings same as cur_word should
+        be ignored and only test_word at exact 1 edit distance should be passed
+        """
+        return True if count == 1 else False
+
+    def word_ladder_naive(self, word_list, source_word, goal_word):
+        from Queue import Queue
+        q = Queue()
+        q.put((source_word, 0))
+        # word_list.append(goal_word)
+        # This can  be improved by O(n) by using set instead of list.
+        try:
+            word_list.remove(source_word)
+        except ValueError:
+            pass
+        while not q.empty():
+            cur_word, cur_distance = q.get()
+            if cur_word == goal_word:
+                return cur_distance+1
+            for test_word in word_list[:]:
+                if self.is_adjacent(test_word, cur_word):
+                    q.put((test_word, cur_distance+1))
+                    word_list.remove(test_word)
+        return -1
+
+    def word_ladder_optimized(self, word_list, source_word, goal_word):
+        """
+        Problem statement:
+         Given two words (beginWord and endWord), and a dictionary's word list,
+        find the length of shortest transformation sequence from beginWord to
+        endWord, such that:
+
+        Only one letter can be changed at a time.
+        Each transformed word must exist in the word list.
+        Note that beginWord is not a transformed word.
+
+        For example,
+
+        Given:
+        beginWord = "hit"
+        endWord = "cog"
+        wordList = ["hot","dot","dog","lot","log","cog"]
+
+        As one shortest transformation is
+            "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+        return its length 5.
+
+        Note:
+
+            Return 0 if there is no such transformation sequence.
+            All words have the same length.
+            All words contain only lowercase alphabetic characters.
+            You may assume no duplicates in the word list.
+            You may assume beginWord and endWord are non-empty and are not the
+            same.
+
+        The trick (different from naive method above) here is to make use of
+        the datastructures (set in this case)
+        For every word in word_set we change it one bit and then check if
+        this one bit changed word is in word_set and since this word_set is
+        as set() we can do so in O(1). So for each_word in word_set (n) we
+        iterate thru its size (w) and for each iteration try to fix 26
+        alphabet chars and see which one works. Therefore in total:
+        Time complexity: O(n*w*26)
+        Space: O(n)
+        """
+        from string import ascii_lowercase
+        word_set = set(word_list)
+        cur_word_list, visited_set = [source_word], set([source_word])
+        cur_distance = 0
+        while cur_word_list:
+            next_word_list = []
+            for cur_word in cur_word_list:
+                if cur_word == goal_word:
+                    return cur_distance + 1
+                for index in xrange(len(cur_word)):
+                    for ch in ascii_lowercase:
+                        candidate_word = cur_word[:index] + ch + cur_word[index+1:]  # noqa
+                        if (candidate_word in word_set and
+                                candidate_word not in visited_set):
+                            visited_set.add(candidate_word)
+                            next_word_list.append(candidate_word)
+            cur_word_list = next_word_list
+            cur_distance += 1
+        return 0
+
+if __name__ == '__main__':
+    # source_word, goal_word, word_dict = "hit", "cog", ["hot", "dot", "dog",
+    #                                                    "lot", "log", "cog"]
+    # source_word, goal_word, word_dict = "a", "c", ["a", "b", "c"]
+    # print Solution().word_ladder(word_dict, source_word, goal_word)
+    # source_word, goal_word, word_dict = "a", "c", ["a", "b", "c"]
+    source_word, goal_word, word_dict = "hit", "cog", ["hot", "dot", "dog",
+                                                       "lot", "log"]
+    print Solution().word_ladder_optimized(word_dict, source_word, goal_word)
---
+++ zero_one_multiple.py
@@ -0,0 +1,95 @@
+"""
+PS: Smallest Multiple With 0 and 1
+You are given an integer N. You have to find smallest multiple of N which
+consists of digits 0 and 1 only. Since this multiple could be large, return
+it in form of a string.
+
+Note:
+- Returned string should not contain leading zeroes.
+
+For example,
+
+For N = 55, 110 is smallest multiple consisting of digits 0 and 1.
+For N = 2, 10 is the answer.
+
+Algo:
+You need to search for the smallest number to multiply N by.
+
+I would construct the number incrementally, starting from the least significant
+digit.
+
+Supposing N=7. What are the possible least significant digits of the
+multiplying number? It will be a number which, when you multiply by 7, will
+have a result with the least significant digit of 0 or 1.
+If you try the numbers from 0-9, it can only be '0' or 3'.
+
++-------+--------+------+
+| Digit | Result | Pass |
++-------+--------+------+
+| 0     |  0     | Yes  |
+| 1     |  7     | No   |
+| 2     | 14     | No   |
+| 3     | 21     | Yes  |
+| 4     | 28     | No   |
+| 5     | 35     | No   |
+| 6     | 42     | No   |
+| 7     | 49     | No   |
+| 8     | 56     | No   |
+| 9     | 63     | No   |
+*-------*--------*------*
+
+Then you try the second least significant digit. You will now try 00, 10, 20,
+30, 40, 50, 60, 70, 80, 90 and 03,13,23,43,53,63,73,83,93.
+The successful candidates will be the ones when, multiplied by 7, produce a
+number in which the two least significant digits are 0 or 1.
+You are left with '43', '30', '00', and '01'.
+
+Repeat this process with the 3rd digit, finding the number which produces a
+multiple with 3 least significant digits meeting the qualities.
+
+During the process you will find a number in which ALL of the digits meet the
+qualities, and that's your answer. In the case of N=7, you've found it by the
+3rd digit. (7 * 143 == 1001).
+
+"""
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def all_zero_one(self, result):
+        for digit in str(result):
+            if digit not in ['0', '1']:
+                return False
+        return True
+
+    def zero_one_multiple(self, multiplicand):
+        multiplier_list = ['']
+        iteration_index = 1
+        while multiplier_list:
+            print 'multiplier_list', multiplier_list
+            print 'iteration_index', iteration_index
+            new_multiplier_list = []
+            for num in multiplier_list:
+                for unit_place in xrange(10):
+                    new_multiplier_list.append(
+                        int(str(unit_place) + str(num)))
+            multiplier_list = new_multiplier_list
+            new_multiplier_list = []
+            for multiplier in multiplier_list:
+                result = multiplier * multiplicand
+                if result and self.all_zero_one(result):
+                    return str(result)
+                try:
+                    if result and str(result)[-iteration_index] in ['0', '1']:
+                        new_multiplier_list.append(multiplier)
+                except:
+                    import time
+                    time.sleep(200)
+            multiplier_list = new_multiplier_list
+            iteration_index += 1
+
+if __name__ == '__main__':
+    print Solution().zero_one_multiple(479)  # 100111
+    print Solution().zero_one_multiple(325)  # 100100
