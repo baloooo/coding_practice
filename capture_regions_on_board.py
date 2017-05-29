e91079da6fea702eef9cbc9a977e2b5ee2d7e411
baloooo <discoverx1@gmail.com>
2017-05-29 18:43:25 +0900

    complete capture regions

---
+++ capture_regions_on_board.py
@@ -0,0 +1,96 @@
+"""
+"""
+
+
+class Solution:
+    """
+    :type board: List[List[str]]
+    :rtype: void Do not return anything, modify board in-place instead.
+    https://discuss.leetcode.com/topic/18706/9-lines-python-148-ms
+    """
+
+    def __init__(self):
+        pass
+
+    def bfs(self, board, row, col):
+        from Queue import Queue
+        bfs_q = Queue()
+        bfs_q.put((row, col))
+        while not bfs_q.empty():
+            cur_row, cur_col = bfs_q.get()
+            if (cur_row < 0 or cur_row >= len(board) or cur_col < 0 or
+                    cur_col >= len(board[0])):
+                continue
+            if board[cur_row][cur_col] == 'O':
+                board[cur_row][cur_col] = '1'
+                bfs_q.put((cur_row+1, cur_col))
+                bfs_q.put((cur_row-1, cur_col))
+                bfs_q.put((cur_row, cur_col+1))
+                bfs_q.put((cur_row, cur_col-1))
+
+    def solve(self, board):
+        """
+        Algo:
+            a) First, check the four border of the matrix.
+               If there is a element is 'O', alter it and all its neighbor
+               'O' elements to '1'.
+            b) Alter all the 'O' to 'X'
+            c) At last,alter all the '1' to 'O'
+
+        """
+        # check borders
+        col_len = len(board[0])-1
+        # North border
+        for col, ch in enumerate(board[0]):
+            if ch == 'O':
+                # iteratively convert all 0 to 1 attached to this 0
+                self.bfs(board, 0, col)
+        # East border
+        for row_num, row in enumerate(board):
+            if row[col_len] == 'O':
+                self.bfs(board, row_num, col_len)
+        # South border
+        for col, ch in enumerate(board[-1]):
+            if ch == 'O':
+                # iteratively convert all 0 to 1 attached to this 0
+                self.bfs(board, col_len, col)
+        # West border
+        for row_num, row in enumerate(board):
+            if row[0] == 'O':
+                self.bfs(board, row_num, 0)
+        # Alter all '0' to 'X'
+        for row in xrange(len(board)):
+            for col in xrange(len(board[0])):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+        # Revert back all '1' to '0'
+        for row in xrange(len(board)):
+            for col in xrange(len(board[0])):
+                if board[row][col] == '1':
+                    board[row][col] = 'O'
+        print 'result board'
+        for row in board:
+            print row
+        print 'expected'
+        res_board = ["XOXOXO","OXXXXX","XXXXXO","OXOXOX"]
+        for row in res_board:
+            print row
+if __name__ == '__main__':
+    board = [['X', 'X', 'O', 'X'], ['X', 'O', 'O', 'X'], ['X', 'X', 'O', 'X'],
+             ['X', 'O', 'X', 'X']]
+    inp = ["XOXOXO","OXOXOX","XOXOXO","OXOXOX"]
+    board = [[ch for ch in row] for row in inp]
+    # board = [ch for ch in row for row in inp]
+    for row in board:
+        print row
+
+    test_cases = [
+        (board, True),
+    ]
+    for test_case in test_cases:
+        res = Solution().solve(test_case[0])
+        if res == test_case[1]:
+            print "Passed"
+        else:
+            print "Failed: Test case: {0} Got {1} Expected {2}".format(
+                test_case[0], res, test_case[1])
--- word_search_board.py
+++ word_search_board.py
@@ -21,52 +21,139 @@ word = "ABCB", -> returns false.
 
 class Solution:
 
-    def dfs(self, cur_row, cur_col, word_index):
+    def dfs(self, cur_row, cur_col, word_index, path):
+        """
+        Added path just for more clarity to check the path taken
+        by algo.
+        """
+        if word_index == len(self.word):
+            print 'final_path is', path
+            return True
         if (cur_row < 0 or cur_row >= len(self.grid) or
                 cur_col < 0 or cur_col >= len(self.grid[0])):
             return False
-        if word_index == len(self.word):
-            import ipdb; ipdb.set_trace()
-            return True
         if self.word[word_index] == self.grid[cur_row][cur_col]:
-            self.path.append((cur_row, cur_col))
-            return(
-                self.dfs(cur_row, cur_col+1, word_index+1) or
-                self.dfs(cur_row, cur_col-1, word_index+1) or
-                self.dfs(cur_row+1, cur_col, word_index+1) or
-                self.dfs(cur_row-1, cur_col, word_index+1))
-        return False
+            self.visited.add((cur_row, cur_col))
+            if (cur_row, cur_col+1) not in self.visited:
+                if self.dfs(cur_row, cur_col+1, word_index+1, path + [(cur_row, cur_col+1)]):  # noqa
+                    return True
+                """
+                remove this co-ordinate from visited so that this can be used
+                in some other path.
+                """
+                self.visited.discard((cur_row, cur_col+1))
+            if (cur_row, cur_col-1) not in self.visited:
+                if self.dfs(cur_row, cur_col-1, word_index+1, path + [(cur_row, cur_col-1)]):  # noqa
+                    return True
+                self.visited.discard((cur_row, cur_col-1))
+            if (cur_row+1, cur_col) not in self.visited:
+                if self.dfs(cur_row+1, cur_col, word_index+1, path + [(cur_row+1, cur_col)]):  # noqa
+                    return True
+                self.visited.discard((cur_row+1, cur_col))
+            if (cur_row-1, cur_col) not in self.visited:
+                if self.dfs(cur_row-1, cur_col, word_index+1, path + [(cur_row-1, cur_col)]):  # noqa
+                    return True
+                self.visited.discard((cur_row-1, cur_col))
+            else:
+                return False
 
-    def word_in_grid(self, grid, word):
-        self.path = []
-        self.grid = [[char for char in each] for each in grid]
-        for row in self.grid:
-            print row
-        self.word = word
-        if len(self.word) > len(self.grid)*len(self.grid[0]):
-            return False
+    def exist(self):
         for cur_row in xrange(len(self.grid)):
             for cur_col in xrange(len(self.grid[0])):
                 if self.grid[cur_row][cur_col] == self.word[0]:
-                    if self.dfs(cur_row, cur_col, 0):
+                    self.visited = set()
+                    if self.dfs(cur_row, cur_col, 0, [(cur_row, cur_col)]):
                         print self.path
                         return True
         return False
 
+    def word_in_grid(self, grid, word_list):
+        self.path = []
+        self.grid = [[char for char in each] for each in grid]
+        for row in self.grid:
+            print row
+        # if len(self.word) > len(self.grid)*len(self.grid[0]):
+        #     return False
+        result = []
+        for word in word_list:
+            self.word = word
+            if self.exist():
+                result.append(word)
+        return result
+
 
 if __name__ == '__main__':
-    # word, grid = "ABCE", [
-    #       ['A', 'B', 'C', 'E'],
-    #       ['S', 'F', 'C', 'S'],
-    #       ['A', 'D', 'E', 'E']
-    # ]
-    # word, grid = "SB", [
-    #       ['A'],
-    #       ['S'],
-    #       ['B']
-    # ]
-    # word, grid = "ABCESEEEFS", [
-    #     ["ABCE", "SFES", "ADEE"]
-    # ]
-    word, grid = "BCDCB", [ "FEDCBECD", "FABBGACG", "CDEDGAEC", "BFFEGGBA", "FCEEAFDA", "AGFADEAC", "ADGDCBAA", "EAABDDFF" ]
-    print Solution().word_in_grid(grid, word)
+    test_cases = [
+        # ((["ABCE", "SFCS", "ADEE"], "ABCCED"), True),
+        # ((["a"], "a"), True),
+        # ((["aa"], "aa"), True),
+        ((["ab", "cd"], ["acdb", "ca"]), ["acdb", "ca"]),
+        # ((["ABCE", "SFES", "ADEE"], "ABCESEEEFS"), True),
+    ]
+    for test_case in test_cases:
+        res = Solution().word_in_grid(test_case[0][0], test_case[0][1])
+        if res == test_case[1]:
+            print "Passed"
+        else:
+            print "Failed: Test case: {0} Got {1} Expected {2}".format(
+                test_case[0], res, test_case[1])
+
+
+class Solution2:
+    """
+    The same letter cell may be used more than once in the grid.
+    """
+
+    def dfs(self, cur_row, cur_col, word_index, path):
+        """
+        Added path just for more clarity to check the path taken
+        by algo.
+        """
+        if word_index == len(self.word):
+            print 'final_path is', path
+            return True
+        if (cur_row < 0 or cur_row >= len(self.grid) or
+                cur_col < 0 or cur_col >= len(self.grid[0])):
+            return False
+        if self.word[word_index] == self.grid[cur_row][cur_col]:
+            # print 'path:', path
+            return (
+                self.dfs(cur_row, cur_col+1, word_index+1, path + [(cur_row, cur_col+1)]) or  # noqa
+                self.dfs(cur_row, cur_col-1, word_index+1, path + [(cur_row, cur_col-1)]) or  # noqa
+                self.dfs(cur_row+1, cur_col, word_index+1, path + [(cur_row+1, cur_col)]) or  # noqa
+                self.dfs(cur_row-1, cur_col, word_index+1, path + [(cur_row-1, cur_col)]) or  # noqa
+                False)
+
+    # @param A : list of strings
+    # @param B : string
+    # @return an integer
+    def exist(self, grid, word):
+        self.path = []
+        self.grid = [[char for char in each] for each in grid]
+        # for row in self.grid:
+        #     print row
+        self.word = word
+        if len(self.word) > len(self.grid)*len(self.grid[0]):
+            return 0
+        for cur_row in xrange(len(self.grid)):
+            for cur_col in xrange(len(self.grid[0])):
+                if self.grid[cur_row][cur_col] == self.word[0]:
+                    if self.dfs(cur_row, cur_col, 0, [(cur_row, cur_col)]):
+                        # print self.path
+                        return 1
+        return 0
+
+# if __name__ == '__main__':
+#     test_cases = [
+#         ((["FEDCBECD", "FABBGACG", "CDEDGAEC", "BFFEGGBA", "FCEEAFDA",
+#            "AGFADEAC", "ADGDCBAA", "EAABDDFF"], "BCDCB"), 1),
+#         ((["CDGCG", "CDAAA", "ECDDB", "FBGEC", "BEBBF", "DFGEF", "CGGAD",
+#            "AACGG", "BDGGB"], "BABABC"), 1),
+#         ]
+#     for test_case in test_cases:
+#         res = Solution2().exist(test_case[0][0], test_case[0][1])
+#         if res == test_case[1]:
+#             print "Passed"
+#         else:
+#             print "Failed: Test case: {0} Got {1} Expected {2}".format(
+#                 test_case[0], res, test_case[1])
