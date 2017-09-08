84de45e243beb0d3063ddc82dbed32120473a7df
baloooo <discoverx1@gmail.com>
2017-09-08 17:42:17 +0900

    incomplete in_memory_file_system

---
+++ design/oo_exercises/atc.py
@@ -0,0 +1,119 @@
+"""
+Current implementation allows you to create airplane_objects from input stream
+(any source), feed these airplanes to ATC object which can then schedule
+takeoffs/ from the runway based on coded priorities.
+Todo:
+    * Add inflight support for airplanes
+    * Airplanes can enter in ATC's airspace and leave without landing therefore
+      allow them to trespass your airspace, preventing any collisions
+    * Allow interested airplanes to land/takeoff
+"""
+import Queue
+from enum import Enum
+
+plane_attribute_map = {
+    'type': {1: 'Government', 2: 'Passenger', 3: 'Cargo', 4: 'Charter'},
+    'size': {1: 'Large', 2: 'medium', 3: 'small'},
+}
+"""
+Note:  The usual restrictions for pickling apply: picklable enums must be
+       defined in the top level of a module, since unpickling requires them to
+       be importable from that module. Note With pickle protocol version 4 it
+       is possible to easily pickle enums nested in other classes.
+"""
+
+
+class PlaneType(Enum):
+    GOV = 1
+    PASSENGER = 2
+    CARGO = 3
+    CHARTER = 4
+
+
+class PlaneSize(Enum):
+    Large = 1
+    Medium = 2
+    Small = 3
+
+
+class ATC(object):
+    """
+    Todo:
+        * ATC for currently in flight airplanes
+        * ATC for landing/take-off permissions
+    """
+    def __init__(self):
+        self.queue = Queue.PriorityQueue()
+
+    def add_airplane(self, airplane):
+        """
+        Adds airplane to be scheduled for departure
+        """
+        self.queue.put(airplane)
+
+
+class Airplane(object):
+    """
+    Todo:
+        * Check whether only __lt__ is ok for priority queue implementation
+        * Use Enum wherever required for plane types/sizes etc.
+        * Use Queue with Threads:
+            https://pymotw.com/2/Queue/
+            https://www.troyfawkes.com/learn-python-multithreading-queues-basics/
+            Put airplane objects to queues, and threads/processes should just
+            pick them and serve them accordingly.
+
+    Place to start:
+        https://stackoverflow.com/questions/28465411/priority-queue-doesnt-recognize-cmp-function-in-python?rq=1
+    https://stackoverflow.com/questions/28435104/queue-with-multi-field-sorting-on-priority-and-time
+    """
+    def __init__(self, plane_type, plane_size, arrival_time):
+        self.type = plane_type
+        self.size = plane_size
+        self.arrival_time = arrival_time
+
+    def __lt__(self, other):
+        """
+        Compare based on plane types, if plane types are same comparison based
+        on plane sizes. Lastly the plane which comes first departs first,
+        if everything else is same.
+        """
+        if (self.type < other.type or self.size < other.size or
+                self.arrival_time < other.arrival_time):
+            return True
+        else:
+            return False
+
+    # def __eq__(self, other):
+    #     # if (self.type == other.type and self.size == other.size and
+    #     #         self.arrival_time == other.arrival_time):
+    #     #     return True
+    #     if self.type != other.type:
+    #         return False
+    #     elif self.size != other.size:
+    #         return False
+    #     elif self.arrival_time != other.arrival_time:
+    #         return False
+    #     else:
+    #         return True
+
+    def __str__(self):
+        return "Airplane with type: %s, size: %s, and arrival_time: %s" % (
+            plane_attribute_map['type'][self.type],
+            plane_attribute_map['size'][self.size],
+            self.arrival_time)
+
+if __name__ == '__main__':
+    queue = Queue.PriorityQueue()
+    """
+    Higher the priority, lower the number
+    types: [4: charted, 3: cargo, 2:passenger, 1: Government]
+    sizes: [3: small, 2: medium, 1: large]
+    arrival_times: These are just time stamps
+    """
+    stream = [(3, 3, 2), (3, 1, 1), (3, 3, 1), (1, 3, 10), (3, 3, 1)]
+    for plane in stream:
+        queue.put(Airplane(plane[0], plane[1], plane[2]))
+    while not queue.empty():
+        cur_plane = queue.get()
+        print cur_plane
--- word_squares.py
+++ word_squares.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 """
 Given a set of words (without duplicates), find all word squares you can build
 from them.
@@ -26,33 +27,42 @@ from collections import defaultdict
 
 class TrieNode:
     def __init__(self):
-        self.children = defaultdict(TrieNode())
+        self.children = defaultdict(TrieNode)
         self.is_word = False
 
 
 class Solution:
+    """
+    Todo: compltete todo for below input (possibly using recursion)
+    https://github.com/kamyu104/LeetCode/blob/master/Python/word-squares.py
+    https://discuss.leetcode.com/topic/66636/python-understandable-solution-i-guess/2
+    """
     def create_trie(self, word_list):
         root = TrieNode()
         for word in word_list:
             cur_root = root
             for char in word:
                 cur_root = cur_root.children[char]
+            cur_root.is_word = True
         return root
 
-    def dfs(self, root):
+    def dfs(self, root, prefix):
         if root is None:
             return
         if root.is_word:
-            return root
+            self.prefixes.append(prefix[:])
+        for key, child in root.children.items():
+            self.dfs(child, prefix+[key])
 
-
-    def find_word_w_prefix(self, root, prefix):
+    def search_prefix(self, root, prefix):
         for i in xrange(len(prefix)):
             root = root.children[prefix[i]]
         # either store all prefixes on node
         # return root.prefixes
         # run DFS from here to find all prefixes
-        self.dfs(root)
+        self.prefixes = []
+        self.dfs(root, prefix)
+        return self.prefixes
 
     def find_word_square(self, word_list):
         """
@@ -68,18 +78,26 @@ class Solution:
             # this assumes atleast 2 char
             cur_prefix = [word_list[0][1]]
             for index in xrange(1, len(word_list[0])):
-                next_word = self.find_word_w_prefix(root, cur_prefix)
-                possible_word_arrngmnt.append(next_word)
-                cur_prefix.append(possible_word_arrngmnt[index][len(possible_word_arrngmnt)])  # noqa
+                next_word = self.search_prefix(root, cur_prefix)
+                if not next_word:
+                    break
+                # Todo: currently assuming there would be only one prefix
+                possible_word_arrngmnt.append(''.join(next_word[0]))
+                if len(possible_word_arrngmnt) != len(word_list[0]):
+                    cur_prefix = [each[len(possible_word_arrngmnt)] for each in possible_word_arrngmnt]
+            else:
+                final_word_lists.append(possible_word_arrngmnt[:])
             word_list[cur_index], word_list[0] = word_list[0], word_list[cur_index]  # noqa
         return final_word_lists
 
 if __name__ == '__main__':
     test_cases = [
-        (["area", "lead", "wall", "lady", "ball"], 'sol1'),
+        # (["area", "lead", "wall", "lady", "ball"], 'sol1'),
+        (["abat","baba","atan","atal"] , 'sol1'),
+        # (["aaa", "aaa", "aaa", "aaa", "aaa"], 'sol1'),
     ]
     for test_case in test_cases:
-        res = Solution().my_func(test_case[0])
+        res = Solution().find_word_square(test_case[0])
         if res == test_case[1]:
             print "Passed"
         else:
