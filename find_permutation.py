ff8f2d28f3bafeeb8fe1e37060d8967f9d1d796c
baloooo <discoverx1@gmail.com>
2017-09-14 17:39:01 +0900

    find_permutations;

---
+++ find_permutation.py
@@ -0,0 +1,46 @@
+'''
+https://leetcode.com/articles/find-permutation/
+'''
+def sol_stack(pattern):
+    # Time: O(n) Space: O(n)
+    arr = range(1, len(pattern)+2)
+    stack, j, res = [], 0, []
+    for i, s in enumerate(pattern):
+        stack.append(arr[i])
+        if s == 'I':
+            while (stack):
+                res.append(stack.pop())
+    stack.append(arr[-1])
+    while stack:
+        res.append(stack.pop())
+    return res
+
+
+def reverse(arr, i, j):
+    print i, j
+    while i <= j:
+        try:
+            arr[i], arr[j] = arr[j], arr[i]
+        except:
+            import ipdb; ipdb.set_trace()
+        i += 1
+        j -= 1
+    return arr
+
+
+def sol_rev(pattern):
+    # Time: O(n) Space: O(1)
+    arr = range(1, len(pattern)+2)
+    i = j = 0
+    while i < len(pattern):
+        i = j
+        while j < len(pattern) and pattern[j] == 'D':
+            j += 1
+        reverse(arr, i, j)
+        j += 1
+    return arr
+
+if __name__ == '__main__':
+    pattern = 'DDIIIID'
+    print sol_stack(pattern)
+    print sol_rev(pattern)
