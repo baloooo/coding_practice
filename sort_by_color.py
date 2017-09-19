3601a6abd1909e64de8fce1265adcdacc1f9e36e
baloooo <discoverx1@gmail.com>
2017-09-19 23:03:47 +0900

    sort by color

--- sort_by_color.py
+++ sort_by_color.py
@@ -1,16 +1,19 @@
-def sortColors(nums):
-    i = j = 0
-    for k in xrange(len(nums)):
-        v = nums[k]
-        nums[k] = 2
-        if v < 2:
-            nums[j] = 1
-            j += 1
-        if v == 0:
-            nums[i] = 0
-            i += 1
-
-if __name__ == '__main__':
-    arr = [2, 1, 0]
-    sortColors(arr)
-    print arr
+class Solution(object):
+    def sortColors(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+	Idea: https://discuss.leetcode.com/topic/36160/python-o-n-1-pass-in-place-solution-with-explanation/5
+        """
+        red = white = 0
+        blue = len(arr)-1
+        while white <= blue:
+            if arr[white] == 0:
+                arr[white], arr[red] = arr[red], arr[white]
+                white += 1
+                red += 1
+            elif arr[white] == 1:
+                white += 1
+            else:
+                arr[white], arr[blue] = arr[blue], arr[white]
+                blue -= 1
