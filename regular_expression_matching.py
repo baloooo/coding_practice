9ab4fb9823e8b645f0d2bacd345ce94865a07cf2
baloooo <discoverx1@gmail.com>
2017-11-10 18:15:41 +0900

    sfdsadf

--- regular_expression_matching.py
+++ regular_expression_matching.py
@@ -26,6 +26,13 @@ class Solution:
         '''
         dp[i][j] is True if text[0..i) matches pattern[0..j), False otherwise.
         Time = Space =  O(len(text)*len(pattern))
+        1. check prev index text and prev index pattern if current index text and pattern are same
+        or pattern has '.' at current index.
+        2. if we've * at current index, there're 2 sub-cases.
+            a) we've zero repetion of pattern[j], in which case it will be p[i][j-2] whatever we
+            had before two indexes before since 'xa*' i.e at index of x
+            b) we've one or more repetition, in which case we check prev matches at P[i-1][j]
+               ....to complete?
         State equations:
             1. P[i][j] = P[i - 1][j - 1], if p[j - 1] != '*' && (s[i - 1] == p[j - 1] || p[j - 1] == '.')
             2. P[i][j] = P[i][j - 2], if p[j - 1] == '*' and the pattern repeats for 0 times
@@ -35,13 +42,12 @@ class Solution:
         dp = [[False for _ in xrange(len(pattern)+1)] for _ in xrange(len(text)+1)]
         dp[0][0] = True
         for i in xrange(len(text)+1):
-            for j in xrange(1, len(pattern)+1):
+            for j in xrange(1, len(pattern)+1): # pattern starts at 1 to take care of a* types
                 if pattern[j-1] == '*':
-                    dp[i][j] = dp[i][j-2] or (i>0 and (text[i-1] == pattern[j-2] or pattern[j-2] == '.')
-                                              and dp[i-1][j])
+                    dp[i][j] = dp[i][j-2] or (
+                        i > 0 and (text[i-1] == pattern[j-2] or pattern[j-2] == '.') and dp[i-1][j])
                 else:
-                    dp[i][j] = i>0 and dp[i-1][j-1] and (
-                        text[i-1] == pattern[j-1] or pattern[j-1] == '.')
+                    dp[i][j] = i>0 and dp[i-1][j-1] and (text[i-1] == pattern[j-1] or pattern[j-1] == '.')
         return dp[len(text)][len(pattern)]
 
 
