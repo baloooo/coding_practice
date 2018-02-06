f51019462fdeb22b705468d8412274c3dea2bdb0
baloooo <discoverx1@gmail.com>
2018-02-06 23:11:00 +0900

    slfkj

--- alien_dictionary.py
+++ alien_dictionary.py
@@ -1,21 +1,27 @@
 import collections
 
 class DirectedGraphNode(object):
-	def __init__(self, val):
-		self.val = val
-		self.neighbors = set() # These are the neighbors this node has directed link towards.
-		self.visited = False
+    def __init__(self, val):
+        self.val = val
+        self.neighbors = set() # These are the neighbors this node has directed link towards.
+        self.visited = False
 
-	def __str__(self):
-		return self.val
+    def __str__(self):
+        return self.val
 
-	def __eq__(self, other):
-		if self.val == other.val:
-			return True
-		else:
-			return False
+    def __eq__(self, other):
+        if self.val == other.val:
+            return True
+        else:
+            return False
 
 class Solution(object):
+    def _create_nodes(self, graph, word):
+        for ch in word:
+            if ch not in graph:
+                node = DirectedGraphNode(ch)
+                graph[ch] = node
+
     def create_graph(self, words):
         """
         creates an adjacency list graph reprsentation of words.
@@ -23,28 +29,25 @@ class Solution(object):
         graph = collections.defaultdict(DirectedGraphNode)
         
         for i in xrange(len(words)):
-        	for j in xrange(i+1, len(words)):
-        		for parent_ch, child_ch in zip(words[i], words[j]):
-        			# Make a directed edge parent_ch -> child_ch
-        			if parent_ch != child_ch:
-        				if parent_ch in graph:
-        					parent_node = graph[parent_ch]
-        				else:
-        					parent_node = DirectedGraphNode(parent_ch)
-        					graph[parent_ch] = parent_node
-        				if child_ch in graph:
-        					child_node = graph[child_ch]
-        				else:
-        					child_node = DirectedGraphNode(child_ch)
-        					graph[child_ch] = child_node
-        				parent_node.neighbors.add(child_node)
-        				break
+            for j in xrange(i+1, len(words)):
+                self._create_nodes(graph, words[i])
+                self._create_nodes(graph, words[j])
+                for parent_ch, child_ch in zip(words[i], words[j]):
+                    # Make a directed edge parent_ch -> child_ch
+                    if parent_ch != child_ch:
+                        graph[parent_ch].neighbors.add(graph[child_ch])
+                        if graph[parent_ch] in graph[child_ch].neighbors:
+                            return False
+                        break
         return graph
         
     def toposort_util(self, node, stack):
+        if node.visited is True:
+            return
+        else:
+            node.visited = True
         for neighbor in node.neighbors:
             if neighbor.visited is False:
-                neighbor.visited = True
                 self.toposort_util(neighbor, stack)
         stack.append(node)
 
@@ -63,11 +66,9 @@ class Solution(object):
         :type words: List[str]
         :rtype: str
         """
-        # import ipdb; ipdb.set_trace()
         graph = self.create_graph(words)
-        #import ipdb; ipdb.set_trace()
         if not graph:
-        	return "" # Invalid graph
+            return "" # Invalid graph
         return self.toposort(graph)
 
 if __name__ == '__main__':
@@ -78,5 +79,8 @@ if __name__ == '__main__':
 	  "ett",
 	  "rftt"
 	]
-	print "Expected o/p: wertf"
+        words = ["z", "x", "z"]
+        words = ["z", "z"]
+        words = ["ab", "adc"]
+        print "Expected O/P: abcd"
 	print "Got o/p: %s" % Solution().alienOrder(words)
