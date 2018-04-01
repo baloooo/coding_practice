1aa45972e3cbb0de3f24240f234858c8fde9d234
baloooo <discoverx1@gmail.com>
2018-04-01 22:46:02 +0900

    salfkj

--- palindrome.py
+++ palindrome.py
@@ -3,13 +3,14 @@ def is_palindrome(self, s):
     Idea: https://discuss.leetcode.com/topic/22479/python-in-place-two-pointer-solution/8
     '''
     # s = s.lower()
+    if len(s) < 2: return True
     l, r = 0, len(s)-1
     while l < r:
-        while l < r and not s[l].isalphanum():
+        while l < r and not s[l].isalnum():
             l += 1
-        while l < r and not s[r].isalphanum():
+        while l < r and not s[r].isalnum():
             r -= 1
-        if s[l] != s[r] and s[l].lower() != s[r].lower():
+        if s[l].lower() != s[r].lower():
             return False
         l += 1
         r -= 1
