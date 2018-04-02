cc6c6a200d99faaa3b60b21a389085a2bcf817f9
baloooo <discoverx1@gmail.com>
2018-04-02 23:15:49 +0900

    s;fkslfj

--- binary_addition.py
+++ binary_addition.py
@@ -41,6 +41,43 @@ def binary_add(x, y):
         return '1' + ''.join(net_sum)
     return ''.join(net_sum)
 
+
+def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+	same as above just another way of writing this
+        """
+        # (a, b, cin): (sum, cout)
+        res = []
+        add = {
+            ('0', '0', '0'): ('0', '0'),
+            ('0', '0', '1'): ('1', '0'),
+            ('0', '1', '0'): ('1', '0'),
+            ('0', '1', '1'): ('0', '1'),
+            ('1', '0', '0'): ('1', '0'),
+            ('1', '0', '1'): ('0', '1'),
+            ('1', '1', '0'): ('0', '1'),
+            ('1', '1', '1'): ('1', '1'),
+        }
+        if len(a) != len(b):
+            if len(a) < len(b):
+                a = '0'*(len(b)-len(a)) + a
+            else:
+                b = '0'*(len(a)-len(b)) + b
+        i = j = len(a)-1
+        cin = '0'
+        while i >= 0 and j >= 0:
+            # print 'i: %s, j: %s' % (i, j)
+            cur_sum, cin = add[(a[i], b[j], cin)]
+            res.append(cur_sum)
+            i -= 1
+            j -= 1
+        if cin == '1':
+            res.append(cin)
+        return ''.join(reversed(res))
+
 if __name__ == '__main__':
     a = '1001'
     b = '10'
