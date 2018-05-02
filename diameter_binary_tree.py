371b73340508a40460076d0cc5ee57ded79f6b3a
baloooo <discoverx1@gmail.com>
2018-05-02 17:54:43 +0900

    slfkjsdf

--- diameter_binary_tree.py
+++ diameter_binary_tree.py
@@ -2,6 +2,34 @@
 from tree_base import level_order_array_to_tree
 
 
+class Solution(object):
+	'''
+	Idea: For every node,
+		length of longest path which pass it = MaxDepth of its left subtree + MaxDepth of its right subtree.
+	therefore calculate longest path which passes each node and match it with longest seen path until now.
+	Now return the max depth of left and right subtree as the max can be used by a node above to make
+	a longest node path.
+
+	https://leetcode.com/problems/diameter-of-binary-tree/discuss/101132/Java-Solution-MaxDepth
+	'''
+    def dfs(self, root):
+        if root is None: return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        cur_node_max_len = left+right
+        self.max_len = max(self.max_len, cur_node_max_len)
+        return 1 + max(left, right)
+
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.max_len = 0
+        self.dfs(root)
+        return self.max_len
+
+
 class Solution:
     def __init__(self):
         self.left_most_point = 0
